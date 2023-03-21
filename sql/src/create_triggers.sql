fa88518d6b738f21eab72697e64ebda260b19bf1
nuharahman9 <nrahm009@ucr.edu>
2023-03-21 16:58:33 -0700

    room repair date trigger

--- sql/src/create_triggers.sql
+++ sql/src/create_triggers.sql
@@ -21,15 +21,15 @@ CREATE OR REPLACE FUNCTION set_date()
 RETURNS "trigger" AS 
 $BODY$
 BEGIN 
-	NEW.updatedOn = GETDATE();  
+	NEW.repairDate = GETDATE();  
 	RETURN NEW; 
 END; 
 $BODY$
 LANGUAGE plpgsql VOLATILE; 
 
-DROP TRIGGER IF EXISTS roomupdate_trigger ON RoomUpdatesLog;
+DROP TRIGGER IF EXISTS roomrepair_trigger ON RoomRepairs;
 CREATE TRIGGER roomupdate_trigger
 BEFORE INSERT 
-ON RoomUpdatesLog 
+ON RoomRepairs 
 FOR EACH ROW 
-EXECUTE PROCEDURE set_timestamp(); 
+EXECUTE PROCEDURE set_date(); 
